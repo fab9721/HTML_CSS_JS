f996374ffdb7fe9fbad263c15595059c6e6bbfa1
fab9721 <fcb_97_13@hotmail.com>
2020-11-16 17:30:54 -0500

    Proyectos Personales

---
+++ menu.js
@@ -0,0 +1,9 @@
+const Menu = document.querySelector('.Menu');
+const opcion = document.querySelector('.menu-navegacion');
+
+console.log('Menu')
+console.log('opcion')
+
+Menu.addEventListener('click', ()=> {
+    alert("click")
+})
